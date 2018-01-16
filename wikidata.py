f3488483924dcb5db3441f732ff920cb60a3dc24
Pēteris Paikens <peterisp@gmail.com>
2018-01-16 13:31:15 +0200

    Some more wikidata processing

--- wikidata.py
+++ wikidata.py
@@ -1,8 +1,12 @@
 # Wikidata dump import
 # Taken from experimental wikidata exports at http://78.46.100.7/wikidata/20180101/ , not the full (large) wikidata dump
-import re
+import re, json
+from collections import namedtuple
 
 wikidata_folder = 'D:\Robertraksts\\'
+wikidata_folder = '../'
+instancetype_filename = 'wikidatawiki-20180101-instance-types-transitive.ttl'
+labels_filename = 'wikidatawiki-20180101-labels.ttl'
 goodtypes = set(['http://schema.org/Place', 'http://schema.org/Person', 'http://schema.org/Organization'])
 
 def load_wikidata_types(filename):
@@ -26,8 +30,9 @@ def load_wikidata_types(filename):
 				category = 'loc'
 
 			entity_types[entity_id] = category
-			break
 	return entity_types
+
+Entity = namedtuple('Entity', ['name', 'uri', 'type', 'aliases', 'inflections'])
 
 def load_wikidata_entities(filename, entity_types):
 	entities = {}
@@ -47,15 +52,37 @@ def load_wikidata_entities(filename, entity_types):
 			category = entity_types.get(entity_id)
 			if category == None:
 				continue
-			print(entity_id, label, category)
-			break
 
+			entity = entities.get(entity_id)
+			if entity == None:
+				entity = Entity(label, uri, category, [label], None)
+			else:
+				entity.aliases.append(label)
+				print(entity)
+			entities[entity_id] = entity
+	return entities.values()
 
+def inflect_entities(entities):
+	for entity in entities:				
+		inflections = inflectEntity(name, category)
+		aliases = set(name)
+		for key, inflection in inflections.items():
+			if key == 'Dzimte':
+				continue
+			if 'Person' in instancetype:				
+				aliases.update(personAliases(inflection))
+			elif 'Organization' in instancetype:
+				aliases.update(orgAliases(inflection))
+			else:
+				aliases.add(inflection)			
+		aliases = list(aliases)
+	return entities
 
+entity_types = load_wikidata_types(wikidata_folder+instancetype_filename)
+with open('wikidata/entities_noinflect.json', 'w') as f:
+	entities = load_wikidata_entities(wikidata_folder+labels_filename, entity_types)
+	json.dump(list(entities), f, ensure_ascii=False, indent=2)	
 
-entity_types = load_wikidata_types(wikidata_folder+'wikidatawiki-20180101-instance-types-transitive.ttl')
-print(list(entity_types.keys())[0])
-entities = load_wikidata_entities(wikidata_folder+'wikidatawiki-20171220-labels.ttl', entity_types)
 
 
 print('Done!')
