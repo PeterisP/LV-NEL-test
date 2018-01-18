ac405c0f45c08b3af5817d1f9463d4ba7674c79d
Pēteris Paikens <peterisp@gmail.com>
2018-01-18 16:17:06 +0200

    Added alias info from wikidata

--- wikidata.py
+++ wikidata.py
@@ -7,6 +7,7 @@ wikidata_folder = 'D:\Robertraksts\\'
 wikidata_folder = '../'
 instancetype_filename = 'wikidatawiki-20180101-instance-types-transitive.ttl'
 labels_filename = 'wikidatawiki-20180101-labels.ttl'
+alias_filename = 'wikidatawiki-20180101-alias.ttl'
 goodtypes = set(['http://schema.org/Place', 'http://schema.org/Person', 'http://schema.org/Organization'])
 badtypes = set(['http://www.wikidata.org/entity/Q41176', # building
 				'http://www.wikidata.org/entity/Q8928', # constellation
@@ -51,9 +52,9 @@ def load_wikidata_types(filename):
 
 Entity = namedtuple('Entity', ['name', 'uri', 'type', 'aliases', 'inflections'])
 
-def load_wikidata_entities(filename, entity_types):
+def load_wikidata_entities(labels_filename, alias_filename, entity_types):
 	entities = {}
-	with open(filename, encoding='utf-8') as entityfile:
+	with open(labels_filename, encoding='utf-8') as entityfile:
 		for line in entityfile:
 			if line.startswith('#'):
 				continue
@@ -77,29 +78,32 @@ def load_wikidata_entities(filename, entity_types):
 				entity.aliases.append(label)
 				print(entity)
 			entities[entity_id] = entity
+
+	with open(alias_filename, encoding='utf-8') as aliasfile:
+		for line in aliasfile:
+			if line.startswith('#'):
+				continue
+			match = re.match(r'<([^>]+)> <[^>]+> "(.+)"@lv .', line)
+			if not match:
+				continue
+			uri = match.group(1)
+			aliases = match.group(2)
+
+			assert uri.startswith('http://wikidata.dbpedia.org/resource/Q')
+			entity_id = uri[37:]
+			entity = entities.get(entity_id)
+			if entity:
+				aliases = aliases.split(' , ')
+				entity.aliases.extend(aliases)
+
 	return entities.values()
 
-def inflect_entities(entities):
-	for entity in entities:				
-		inflections = inflectEntity(name, category)
-		aliases = set(name)
-		for key, inflection in inflections.items():
-			if key == 'Dzimte':
-				continue
-			if 'Person' in instancetype:				
-				aliases.update(personAliases(inflection))
-			elif 'Organization' in instancetype:
-				aliases.update(orgAliases(inflection))
-			else:
-				aliases.add(inflection)			
-		aliases = list(aliases)
-	return entities
+
 
 entity_types = load_wikidata_types(wikidata_folder+instancetype_filename)
 with open('wikidata/entities_noinflect.json', 'w', encoding='utf-8') as f:
-	entities = load_wikidata_entities(wikidata_folder+labels_filename, entity_types)
+	entities = load_wikidata_entities(wikidata_folder+labels_filename, wikidata_folder+alias_filename, entity_types)
 	json.dump(list(entities), f, ensure_ascii=False, indent=2)	
-
 
 
 print('Done!')
